d27fc080b6f0ffb3de2291940566164b6430294b
christian-lopezxd <00179320@uca.edu.sv>
2023-11-14 20:49:12 -0600

    :sparkles: Adding Logout button

--- TravelPlanner/src/Components/User/Sidebar/Logout/Logout.jsx
+++ TravelPlanner/src/Components/User/Sidebar/Logout/Logout.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router"
+
 const Logout = () => {
+  const navigate = useNavigate()
+  const logOut=()=>{
+        localStorage.removeItem("token")
+        navigate("/")
+        window.location.reload()
+    }
+
+
+
+
   return (
-    <button className="hover:bg-black w-full">
+    <button className="hover:bg-black w-full" onClick={logOut}>
         <div className='flex  items-center justify-center gap-2 text-white font-montserrat font-semibold px-10 py-3 text-3xl text-left'>
          <div><BiLogOut/></div>
         <div className='min-w-[75%]'><h1>Logout</h1></div>
